a4741c68631b96c4e08cb9e244974354ce337e42
Nancy Jo <enciie@gmail.com>
2018-04-16 21:50:21 +0000

    automatically backed up by learn

--- lib/position_taken.rb
+++ lib/position_taken.rb
@@ -1,3 +1,5 @@
 def position_taken?(board, index) # code your #position_taken? method here!
  (board[index] == " " || board[index] == "" || board[index] == nil) ? false: true
 end
+
+# ternary operator 
